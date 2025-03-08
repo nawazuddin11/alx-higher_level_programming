a3c2f6abbe411c7496f47cc546047f417de20fe3
nawazuddin11 <abdulzaifada81@gmail.com>
2025-03-08 02:33:28 -0800

    a class square with method that return the area of the the present square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,11 +4,10 @@
 
 
 class Square:
-     """Represent a square."""
+    """Represent a square."""
 
-    def __init__(self, size =0):
-
-         """Initialize a new square.
+    def __init__(self, size=0):
+        """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
@@ -17,16 +16,10 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-
-        """ instantioating the size attribute """
-
         self.__size = size
-
-    """ method to return the area of the square """
 
     def area(self):
         """Return the current area of the square."""
-
         return (self.__size * self.__size)
 
 
