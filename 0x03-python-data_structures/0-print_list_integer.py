3d9fba2c98edf9230823ca9250ad4ecc9b5a3e97
nawazuddin11 <abdulzaifada81@gmail.com>
2025-05-13 15:59:08 -0700

    a function to print a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
 
-    for i in my_list:
-        print("{}".format(i))
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
 
