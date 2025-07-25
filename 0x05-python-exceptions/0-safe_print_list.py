166b1c099fbd3ea7d356d9a9868f504431379371
Abdullahi muhammad <abdulzaifada81@gmail.com>
2025-07-25 16:54:36 -0700

    Exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    for l in range(x):
-        try:
-            print(l)
+    i = 0
+    printnum = 0
 
-        except ValueError:
+    while i < x:
+        try:
+            print("{!s:}".format(my_list[i]), end="")
+            printnum += 1
+        except:
             pass
+        i += 1
+    print('')
+    return printnum
